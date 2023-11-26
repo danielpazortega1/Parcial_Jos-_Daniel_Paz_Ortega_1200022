1fbc22dc002070a286493d28fcb649924da5edec
danielpazortega1 <danielpazortega31@gmail.com>
2023-11-26 16:22:47 -0600

    PRUEBADECODIGO4

--- PAT_Parcial03/Ejercicio01.h
+++ PAT_Parcial03/Ejercicio01.h
@@ -7,11 +7,14 @@
 class Ejercicio01
 {
 public:
-	Node<int>* mergeLists(Node<Node<int>> lists);
+    // Cambio la firma del método para aceptar un puntero a Node<Node<int>>.
+    Node<int>* mergeLists(Node<Node<int>*>* lists);
 };
 
 struct CompareNode {
-	bool operator()(Node<int>* const& n1, Node<int>* const& n2) {
-		return n1->value < n2->value;
-	}
+    // Este comparador se usa para la priority_queue.
+    bool operator()(Node<int>* const& n1, Node<int>* const& n2) {
+        // Asumiendo que quieres la priority_queue en orden descendente.
+        return n1->value > n2->value;
+    }
 };
